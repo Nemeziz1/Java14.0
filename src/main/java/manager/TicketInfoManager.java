dcc14038bc78b0c9b0f554ba3d86707cdf63de04
Andrey Koryakin <football2331lm@gmail.com>
2020-11-01 22:24:47 +0300

    Method Fixed

--- src/main/java/manager/TicketInfoManager.java
+++ src/main/java/manager/TicketInfoManager.java
@@ -6,29 +6,25 @@ import repository.TicketInfoRepository;
 import java.util.Arrays;
 
 public class TicketInfoManager {
-    private TicketInfoRepository repository;
+    private final TicketInfoRepository repository;
 
     public void save(TicketInfo trip) {
-        int length = trips.length + 1;
-        TicketInfo[] tmp = new TicketInfo[length];
-        System.arraycopy(trips, 0, tmp, 0, trips.length);
-        int lastIndex = tmp.length - 1;
-        tmp[lastIndex] = trip;
-        trips = tmp;
+        repository.add(trip);
     }
 
     public TicketInfoManager(TicketInfoRepository repository) {
         this.repository = repository;
     }
 
-    private TicketInfo[] trips = new TicketInfo[0];
-
     public TicketInfo[] findAll(String from, String to) {
-
-        TicketInfo[] tmp = repository.findAll();
-        for (TicketInfo trip : tmp) {
+        TicketInfo[] trips = new TicketInfo[0];
+        for (TicketInfo trip : repository.findAll()) {
             if (trip.getFrom().equals(from) && trip.getTo().equals(to)) {
-                save(trip);
+                TicketInfo[] tmp = new TicketInfo[trips.length + 1];
+                repository.add(trip);
+                System.arraycopy(trips, 0, tmp, 0, trips.length);
+                tmp[tmp.length - 1] = trip;
+                trips = tmp;
             }
         }
         Arrays.sort(trips);
